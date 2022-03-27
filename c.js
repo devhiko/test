3d89b2d29049cbb9b6afa67ac613a6e7283e63da
devhiko <aydin96tr@gmail.com>
2022-03-27 18:43:52 +0300

    added master branch diff

--- c.js
+++ c.js
@@ -1,3 +1,4 @@
 alert('added new feature a');
 console.log('added new feature b');
 document.getElementsByTagName('h1')[0].style.padding = '20px';
+document.getElementsByTagName('h1')[0].style.fontWeight = 'bold';
