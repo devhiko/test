a677b9e343030813dae5333c3272e1fe92f62a38
devhiko <aydin96tr@gmail.com>
2022-03-27 18:35:59 +0300

    added new line

--- c.js
+++ c.js
@@ -1,2 +1,5 @@
 alert('added new feature a');
 console.log('added new feature b');
+
+document.getElementsByTagName('h1')[0].style.color = 'white';
+document.getElementsByTagName('h1')[0].style.padding = '20px';
